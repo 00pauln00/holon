d4d0e477680f80cbcc238d8a707fe83f49a37aab
akshata0305 <akshata.mahamuni@paroscale.com>
2020-05-19 13:14:08 +0000

    Modified raftconfig.py and raftclient.py

--- raftclient.py
+++ raftclient.py
@@ -1,56 +1,29 @@
 import os
 import subprocess
+from raftconfig import RaftConfig
 
 class RaftClient:
 
-    client_uuid = ''
     raft_uuid = ''
-    ip_address = ''
-    client_port = ''
-    client_proc_obj = {}
+    client_uuid = ''
+    client_no = 0 # Client number e.g 0/1/2
 
     '''
-        Method: generate_client_uuid
-        Purpose: generate UUID using /usr/bin/uuid
-        Parameters:
+    Constructor:
+    Purpose: Initialisation
+    Parameters: @raft_conf_obj: RaftConfig Object to get raft_uuid.
+                @client_no: Client index.
     '''
-    def generate_client_uuid(self):
-        p1 = subprocess.Popen(["/usr/bin/uuid"], stdout=subprocess.PIPE)
-        (stdout, err) = p1.communicate()
-        client_uuid_bytes = stdout.strip()
-        client_uuid = client_uuid_bytes.decode('utf-8')
-        self.client_uuid = client_uuid
+    def __init__(self, raft_conf_obj, client_no):
+        self.raft_uuid = raft_conf_obj.raft_uuid
+        self.client_uuid = raft_conf_obj.client_uuid_arr[client_no]
+        self.client_no = client_no
 
     '''
-        Method: assign_client_params
-        Purpose: assign client parameters
-        Parameters:
+    Method: Get Client UUID
+    Purpose: Get the client UUID for this object
+    Parameters:
     '''
-    def assign_client_params(self, raft_uuid_obj, ip_address, client_port):
-        self.raft_uuid = raft_uuid_obj.raft_uuid
-        self.ip_address = ip_address
-        self.client_port = client_port
-
-    '''
-        Method: prepare_client_conf
-        Purpose: prepare the client config file
-        Parameters:
-    '''
-    def prepare_client_conf(self, server_config_obj):
-        client_conf_path = "%s/%s.raft_client" % (server_config_obj.server_config_path, self.client_uuid)
-        print(client_conf_path)
-        f1 = open(client_conf_path, "w+")
-        f1.write("RAFT              %s\n" % (self.raft_uuid))
-        f1.write("IPADDR            %s\n" % (self.ip_address))
-        f1.write("CLIENT_PORT       %s\n" % (self.client_port))
-        f1.close()
-    '''
-        Method: start_client
-        Purpose: write raft uuid to client conf
-        Parameters:
-    '''
-    def start_client(self, raft_uuid):
-        print(f"start client")
-        client_proc_obj = subprocess.Popen(['/home/pauln/raft-builds/latest/raft-client', '-r', raft_uuid.raft_uuid, '-u', self.client_uuid])
-        self.client_proc_obj = client_proc_obj
-
+    def get_client_uuid(self):
+        print(f"Client UUID is: %s" % (self.client_uuid))
+        return self.client_uuid
--- raftconfig.py
+++ raftconfig.py
@@ -1,8 +1,12 @@
 import os
+import subprocess
 
 class RaftConfig:
 
     server_config_path = ''
+    raft_uuid = ''
+    peer_uuid_arr = [] # Peer UUID array
+    client_uuid_arr = [] # Client UUID array
 
     '''
         Constructor:
@@ -11,6 +15,8 @@ class RaftConfig:
     def __init__(self, server_config_path):
         self.server_config_path = server_config_path
 
+
+
     '''
         Method: export_path
         Purpose: export ctl svc dir
@@ -18,6 +24,117 @@ class RaftConfig:
     '''
     def export_path(self):
         CTL_SVC_DIR = os.environ['NIOVA_LOCAL_CTL_SVC_DIR'] = self.server_config_path
-
         print(f"CTL_SVC_DIR:", CTL_SVC_DIR)
 
+
+
+    '''
+        Method: generate_raft_conf
+        Purpose: Create Raft-Conf file for RAFT UUID and PEER UUID
+        Parameter: @nservers: Number of servers in the cluster.
+                    @ip_address: IP address for the server.
+                    @port: Start port number in the range.
+                    @client_pport: Start client port in the range.
+                    @raft_db_path: Directory path to store the raftdb files
+                    for all servers.
+                
+    '''
+    def generate_raft_conf(self, nservers, ip_address, port, client_port, raft_db_path):
+
+        # Generate RAFT UUID and store it in raftconf object
+        p = subprocess.Popen(["uuid"], stdout=subprocess.PIPE)
+        (stdout, err) = p.communicate()
+        raft_uuid_bytes=stdout.strip()
+        raft_uuid = raft_uuid_bytes.decode('utf-8')
+        self.raft_uuid = raft_uuid
+        
+        # Write RAFT UUID to raft-conf file
+        raft_conf_path = "%s/%s.raft" % (self.server_config_path, self.raft_uuid)
+        print(raft_conf_path)
+        try:
+            raft_fd = open(raft_conf_path, "w+")
+            raft_fd.write("RAFT %s\n" % (self.raft_uuid))
+        except:
+            print("Open Failed for file: %s" % raft_conf_path)
+
+        # Generate PEER UUID and Write it into raft-conf file
+        for i in range(nservers):
+            p1 = subprocess.Popen(["uuid"], stdout=subprocess.PIPE)
+            (stdout, err) = p1.communicate()
+            peer_uuid_bytes=stdout.strip()
+            peer_uuid = peer_uuid_bytes.decode('utf-8')
+            
+            try:
+                raft_fd.write("PEER %s\n" % peer_uuid)
+            except:
+                print("Write failed for file: %s" % raft_conf_path)
+
+            #Prepare peer-conf file.
+            peer_config_path = "%s/%s.peer" % (self.server_config_path, peer_uuid)
+            print(peer_config_path)
+            store_path = "%s/%s.raftdb" % (raft_db_path, peer_uuid)
+            try:
+                with open(peer_config_path, 'w+',encoding = 'utf-8') as f2:
+                    f2.write("RAFT         %s\n"%(self.raft_uuid))
+                    f2.write("IPADDR       %s\n"%(ip_address))
+                    f2.write("PORT         %s\n"%(port))
+                    f2.write("CLIENT_PORT  %s\n"%(client_port))
+                    f2.write("STORE        %s\n"%(store_path))
+            except FileNotFoundError:
+                print("File not found!")
+        
+            port += 1
+            client_port +=1
+
+            #Append peer uuid into an array
+            self.peer_uuid_arr.append(peer_uuid)
+
+        # Close the raft conf file.
+        raft_fd.close()
+
+    '''
+        Method: get_peer_uuid_for_peerno
+        Purpose: Get the peer uuid for the peerno from peer_uuid_array
+    '''
+    def get_peer_uuid_for_peerno(self, peerno):
+        print(f"Peer UUID is %s" % self.peer_uuid_arr[peerno])
+        return self.peer_uuid_arr[peerno]
+
+
+            
+    '''
+        Method: generate_client_conf
+        Purpose: Create client-conf file for Client UUID
+        Parameters: @ip_address: IP address for the client.
+                    @client_port: Client port number.
+    '''
+    def generate_client_conf(self, ip_address, client_port):
+        
+        #Generate Client UUID
+        p2 = subprocess.Popen(["uuid"], stdout=subprocess.PIPE)
+        (stdout, err) = p2.communicate()
+        client_uuid_bytes = stdout.strip()
+        client_uuid = client_uuid_bytes.decode('utf-8')
+
+        # Append the client uuid into an array
+        self.client_uuid_arr.append(client_uuid)
+        
+        #Prepare Client config file
+        client_conf_path = "%s/%s.raft_client" % (self.server_config_path, client_uuid)
+        print(client_conf_path)
+        try:
+            with open(client_conf_path, 'a',encoding = 'utf-8') as f3:
+                f3.write("RAFT              %s\n" % (self.raft_uuid))
+                f3.write("IPADDR            %s\n" % (ip_address))
+                f3.write("CLIENT_PORT       %s\n" % (client_port))
+        except FileNotFoundError:
+                print("File not found!")
+
+
+    '''
+        Method: get_client_uuid_for_clientno
+        Purpose: Get the client uuid for the client number from client_uuid_array
+    '''
+    def get_client_uuid_for_clientno(self, clientno):
+        print(f"CLient UUID is %s" % self.client_uuid_arr[clientno])
+        return self.client_uuid_arr[clientno]
