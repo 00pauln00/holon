a7b0290d9d5b51667c43d4826101a59325c9919d
Vishwambhar Kapare <vishwambhar.kapare@paroscale.com>
2020-07-02 13:54:59 +0000

    Modified changes for pausing more than 1 follower.

--- recipes/leader_self_depose.py
+++ recipes/leader_self_depose.py
@@ -1,8 +1,15 @@
 from holonrecipe import *
 import logging
 import psutil
+'''
+The unpaused follower becomes leader.
+'''
+def leader_self_depose_case1(ctlreq_arr, resume_peer_uuid, orig_term, npeer_start):
+    '''
+    Create object for generic cmds.
+    '''
+    genericcmdobj = GenericCmds()
 
-def leader_self_depose_case1(ctlreq_arr, resume_peer_uuid, orig_term):
     for p in range(npeer_start):
         if ctlreq_arr[p] == None:
             continue
@@ -15,6 +22,10 @@ def leader_self_depose_case1(ctlreq_arr, resume_peer_uuid, orig_term):
         newest_entry_term = raft_json_dict["raft_root_entry"][0]["newest-entry-term"]
         newest_entry_dsize = raft_json_dict["raft_root_entry"][0]["newest-entry-data-size"]
 
+        logging.warning("Check for case#1")
+        logging.warning("leader_uuid: %s voted_for_uuid: %s resume_peer_uuid: %s" % (leader_uuid, voted_for_uuid, resume_peer_uuid))
+        logging.warning("Term: %d orig term: %d" % (term, orig_term[p]))
+        logging.warning("newest_entry_term: %d, newest_entry_data_size: %d" % (newest_entry_term, newest_entry_dsize))
         if leader_uuid != resume_peer_uuid or voted_for_uuid != resume_peer_uuid:
             return False
 
@@ -28,10 +39,15 @@ def leader_self_depose_case1(ctlreq_arr, resume_peer_uuid, orig_term):
             return False
 
     return True
-
-def leader_self_depose_case2(ctlreq_arr, resume_peer_uuid, orig_term):
-    # Unpaused follower started the election, but did not win or win immediately.
-    case2_occurred = True
+'''
+Unpaused follower started the election, but did not win or win immediately.
+'''
+def leader_self_depose_case2(ctlreq_arr, resume_peer_uuid, orig_term, npeer_start):
+    case2_occurred = False
+    '''
+    Create object for generic cmds.
+    '''
+    genericcmdobj = GenericCmds()
 
     for p in range(npeer_start):
         if ctlreq_arr[p] == None:
@@ -41,22 +57,32 @@ def leader_self_depose_case2(ctlreq_arr, resume_peer_uuid, orig_term):
         peer_uuid = raft_json_dict["raft_root_entry"][0]["peer-uuid"]
         if peer_uuid != resume_peer_uuid:
             continue
-        # Get the values for unpaused follower only
 
+        # Get the values for unpaused follower only
         leader_uuid = raft_json_dict["raft_root_entry"][0]["leader-uuid"]
         voted_for_uuid = raft_json_dict["raft_root_entry"][0]["voted-for-uuid"]
         term = raft_json_dict["raft_root_entry"][0]["term"]
+        logging.warning("Check case#2")
+        logging.warning("leader_uuid: %s voted_for_uuid: %s" % (leader_uuid, voted_for_uuid))
+        logging.warning("term: %d, orig term: %d" % (term, orig_term[p]))
 
-        if voted_for_uuid != resume_peer_uuid or term < (orig_term[p] + 2):
-            case2_occurred =  False
+        if leader_uuid == resume_peer_uuid or voted_for_uuid == resume_peer_uuid:
+            if term >= orig_term[p]:
+                case2_occurred =  True
 
         break
 
     #TODO check the values of follower-stat for non-running peers 
     return case2_occurred
-            
-def leader_self_depose_case3(ctlreq_arr, orig_leader, orig_term):
-    #original leader maintains the leadership after the pause
+ 
+'''
+original leader maintains the leadership after the pause.
+'''
+def leader_self_depose_case3(ctlreq_arr, orig_leader, orig_term, npeer_start):
+    '''
+    Create object for generic cmds.
+    '''
+    genericcmdobj = GenericCmds()
     for p in range(npeer_start):
         if ctlreq_arr[p] == None:
             continue
@@ -66,6 +92,9 @@ def leader_self_depose_case3(ctlreq_arr, orig_leader, orig_term):
         voted_for_uuid = raft_json_dict["raft_root_entry"][0]["voted-for-uuid"]
         term = raft_json_dict["raft_root_entry"][0]["term"]
 
+        logging.warning("Checking case#3")
+        logging.warning("leader uuid: %s orig leader: %s" % (leader_uuid, orig_leader))
+        logging.warning("term %d, orig term: %d" % (term, orig_term[p]))
         if leader_uuid != orig_leader:
             return False
 
@@ -74,7 +103,6 @@ def leader_self_depose_case3(ctlreq_arr, orig_leader, orig_term):
             return False
 
     return True
-
 
 class Recipe(HolonRecipeBase):
     name = "leader_self_depose"
@@ -175,12 +203,11 @@ class Recipe(HolonRecipeBase):
         '''
         nfollower_paused = (npeer_start - int(raftconfobj.nservers / 2))
         if nfollower_paused == 1 and npeer_start > 2:
-            nfollower_paused += 1
+            nfollower_paused = nfollower_paused + 1
 
+        logging.warning("Number of followers to paus %d" % nfollower_paused)
+        f = 0
         for p in range(npeer_start):
-            if p == nfollower_paused:
-                break
-
             if orig_state[p] == "follower":
                 logging.warning("Pausing peer uuid: %s" % serverproc[p].process_uuid)
                 rc = serverproc[p].pause_process()
@@ -198,6 +225,9 @@ class Recipe(HolonRecipeBase):
 
                 paused_peer_uuid = serverproc[p].process_uuid
                 paused_follower_uuid[p] = paused_peer_uuid
+                f += 1
+                if f >= nfollower_paused:
+                    break
 
         time_global.sleep(10)
 
@@ -234,84 +264,77 @@ class Recipe(HolonRecipeBase):
         '''
         logging.warning("Resume the peer one at a time")
 
-        for p in range(npeer_start):
-            if p == nfollower_paused:
-                break
+        unpause_case1_occurred = False
+        unpause_case2_occurred = False
+        for p in paused_follower_uuid:
 
-            if serverproc[p].process_uuid == paused_follower_uuid[p]:
-                resume_peer_uuid = paused_follower_uuid[p]
-                logging.warning("Resumeing peer uuid: %s" % resume_peer_uuid)
-                rc = serverproc[p].resume_process()
-                if rc < 0:
-                    logging.error("Failed to resume process")
+            logging.warning("paused follower_uuid: %s, %d" % (paused_follower_uuid[p], p))
+            resume_peer_uuid = paused_follower_uuid[p]
+            logging.warning("Resumeing peer uuid: %s" % resume_peer_uuid)
+            rc = serverproc[p].resume_process()
+            if rc < 0:
+                logging.error("Failed to resume process")
+                return 1
+
+            # Modify peer entry from paused_follower_uuid dictionary
+            paused_follower_uuid[p] = "INVALID_UUID"
+
+            '''
+            After resume of peer, check which unpause case observed.
+            '''
+            for i in range(npeer_start):
+                # Dont send cmd to paused peers or non-running peers.
+                if paused_follower_uuid.get(i) != None and paused_follower_uuid[i] == peer_uuid_arr[i]:
+                    get_ctl[i] = None
+                    continue
+
+                get_ctl[i] = CtlRequest(inotifyobj, "get_all", peer_uuid_arr[i],
+                                        app_uuid,
+                                        inotify_input_base.REGULAR,
+                                        self.recipe_ctl_req_obj_list).Apply()
+
+
+            '''
+            Check if this is Unpause case 1
+            The unpaused follower immediately becomes the leader
+            '''
+            rc = leader_self_depose_case1(get_ctl, resume_peer_uuid, orig_term, npeer_start)
+            if rc == True:
+                logging.warning("Unpause case#1 occurred after pausing peer %s" % resume_peer_uuid)
+                if unpause_case1_occurred == True:
+                    logging.error("Unpase case#1 should not occur twice")
                     return 1
 
-                # Delete the peer entry from paused_follower_uuid dictionary
-                del paused_follower_uuid[p]
+                # Mark unpause case1 occurred.
+                unpause_case1_occurred = True
+                continue
 
-                '''
-                To check if process is resumed
-                '''
-                paused_peer_pid = serverproc[p].process_popen.pid
-                ps = psutil.Process(paused_peer_pid)
-                if ps.status() != "running":
-                    logging.error("Resume failed foruuid : %s" % resume_peer_uuid)
+            '''
+            Unpause Case #2: The unpaused follower started an election cycle but
+            did not win or did not win immediately.
+            Paused peer started the election
+            '''
+            rc = leader_self_depose_case2(get_ctl, resume_peer_uuid, orig_term, npeer_start)
+            if rc == True:
+                logging.warning("Unpause case#2 occurred after pausing peer %s" % resume_peer_uuid)
+                if unpause_case2_occurred == True:
+                    logging.error("Unpase case#2 should not occur twice")
                     return 1
 
+                # Mark unpause case1 occurred.
+                unpause_case2_occurred = True
+                continue
 
-                '''
-                After resume of each peer, check which unpause case observed.
-                '''
-                for i in range(npeer_start):
-                    # Dont send cmd to paused peers.
-                    if peer_uuid_arr[i] == paused_follower_uuid[i]:
-                        get_ctl[i] = None
-                        continue
+            '''
+            Unpause Case #3: The original leader maintains leadership after the unpause. 
+            '''
+            rc = leader_self_depose_case3(get_ctl, orig_leader_uuid[0], orig_term, npeer_start)
+            if rc == True:
+                logging.warning("Unpause case#3 occurred after pausing peer %s" % resume_peer_uuid)
+                continue
 
-                    get_ctl[i] = CtlRequest(inotifyobj, "get_all", peer_uuid_arr[i],
-                                            app_uuid,
-                                            inotify_input_base.REGULAR,
-                                            self.recipe_ctl_req_obj_list).Apply()
-
-
-                '''
-                Check if this is Unpause case 1
-                The unpaused follower immediately becomes the leader
-                '''
-                rc = leader_self_depose_case1(get_ctl, resume_peer_uuid, orig_term)
-                if rc == True:
-                    logging.warning("Unpause case#1 occurred after pausing peer %s" % resume_peer_uuid)
-                    if unpause_case1_occurred == True:
-                        logging.error("Unpase case#1 should not occur twice")
-                        return 1
-
-                    # Mark unpause case1 occurred.
-                    unpause_case1_occurred = True
-                    continue
-
-                '''
-                Unpause Case #2: The unpaused follower started an election cycle but
-                did not win or did not win immediately.
-                Paused peer started the election
-                '''
-                rc = leader_self_depose_case2(get_ctl, resume_peer_uuid, orig_term)
-                if rc == True:
-                    logging.warning("Unpause case#2 occurred after pausing peer %s" % resume_peer_uuid)
-                    if unpause_case2_occurred == True:
-                        logging.error("Unpase case#2 should not occur twice")
-                        return 1
-
-                    # Mark unpause case1 occurred.
-                    unpause_case2_occurred = True
-                    continue
-
-                '''
-                Unpause Case #3: The original leader maintains leadership after the unpause. 
-                '''
-                rc = leader_self_depose_case3(get_ctl, orig_leader_uuid[0], orig_term)
-                if rc == True:
-                    logging.warning("Unpause case#3 occurred after pausing peer %s" % resume_peer_uuid)
-                    continue
+            logging.error("None of the unpause cases occur!")
+            return 1
 
         '''
         Make sure all paused processes are resumed
