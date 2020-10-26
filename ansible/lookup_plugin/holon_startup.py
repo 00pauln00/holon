55e546e2e5c612acb31a33cda3ea26e1cef87780
VishKapare10 <56476658+VishKapare10@users.noreply.github.com>
2020-10-26 18:41:22 +0530

    Fixed issue #56 and changes for port check. (#85)
    * Added code for installing python modules if previously not installed and
    also added check for ports if they are already in use.
    
    * - Modified changes for installing python modules if they are not installed before.
    - Added file 'modules' which has all the modules to be installed.
    
    Co-authored-by: Vishwambhar Kapare <vishwambhar.kapare@paroscale.com>

---
+++ ansible/lookup_plugin/holon_startup.py
@@ -0,0 +1,26 @@
+from ansible.plugins.lookup import LookupBase
+from genericcmd import *
+
+def check_for_ports(conf_params, genericcmdobj):
+    port = int(conf_params['srv_port'])
+    client_port = int(conf_params['client_port'])
+
+    for p in (port, client_port):
+        genericcmdobj.port_check(p)
+
+def install_python_modules(genericcmdobj):
+    genericcmdobj.install_python_modules()
+
+class LookupModule(LookupBase):
+    def run(self, terms, **kwargs):
+
+        config_params_dict = terms[0]
+        startup_check = config_params_dict['startup_check']
+        
+        #Create object for genericcmd
+        genericcmdobj = GenericCmds()
+        if startup_check == "port_check":
+            port_check = check_for_ports(config_params_dict, genericcmdobj)
+        else:
+            install_modules = install_python_modules(genericcmdobj)
+
