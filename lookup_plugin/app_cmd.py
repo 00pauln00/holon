617511d559196d0d418090e492d6298abe45c440
VishKapare10 <vish10kapare@gmail.com>
2021-11-01 11:12:55 +0000

    Modified lookup plugin for go apps.

--- lookup_plugin/app_cmd.py
+++ lookup_plugin/app_cmd.py
@@ -66,18 +66,20 @@ class LookupModule(LookupBase):
         counter = 0
         timeout = 2500
         client_json = "%s/%s/%s.json" % (cluster_params['base_dir'],cluster_params['raft_uuid'],fname)
-        while True:
+        while(1):
             if os.path.exists(client_json):
                 try:
-                    with open(client_json, "r+", encoding="utf-8") as json_file:
-                        request = json.load(json_file)
+                    with open(client_json, "r", encoding="utf-8") as json_file:
+                        if len(json_file.readlines()) != 0:
+                                json_file.seek(0)
+                                request = json.load(json_file)
                 except:
                     return {"status":-1,"msg":"Invalid json format in output file"}
                 break
             else:
                 #Wait, fail at max count
                 counter += 1
-                time.sleep(0.1)
+                time.sleep(0.2)
                 if counter == timeout:
                     return {"status":-1,"msg":"Timeout checking for output file"}
         
