f5bed175b40d1e432da6ed4630a0d1249bd64aff
akshata0305 <akshatamahamuni35@gmail.com>
2022-08-01 08:41:01 +0000

    Changed subprocess.run() to subprocess.Popen()

--- lookup_plugin/nisd_handler.py
+++ lookup_plugin/nisd_handler.py
@@ -163,14 +163,14 @@ def start_niova_block_test(cluster_params, nisd_uuid_to_write, vdev, read_operat
     logging.info("Do write/read operation on nisd by starting niova-block-test")
 
     if sequential_writes == True and integrity_check == False and blocking_process == False:
-        ps = subprocess.run((bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
+        ps = subprocess.Popen([bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
                                    '-u', client_uuid, '-Z', request_size_in_bytes,
-                                   '-q', queue_depth, '-N', num_ops, '-I', '-Q'), stdout=fp, stderr=fp)
+                                   '-q', queue_depth, '-N', num_ops, '-I', '-Q'], stdout=fp, stderr=fp)
 
     elif integrity_check == True and sequential_writes == False and blocking_process == False:
-        ps = subprocess.run((bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
+        ps = subprocess.Popen([bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
                                    '-a', random_seed, '-u', client_uuid, '-Z', request_size_in_bytes,
-                                   '-q', queue_depth, '-N', num_ops, '-I'), stdout=fp, stderr=fp)
+                                   '-q', queue_depth, '-N', num_ops, '-I'], stdout=fp, stderr=fp)
 
     elif blocking_process == True and sequential_writes == False and integrity_check == False:
         ps = subprocess.Popen([bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
@@ -178,11 +178,11 @@ def start_niova_block_test(cluster_params, nisd_uuid_to_write, vdev, read_operat
                                    '-q', queue_depth, '-N', num_ops, '-I', '-Q'], stdout=fp, stderr=fp)
 
     else:
-        ps = subprocess.run((bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
+        ps = subprocess.Popen([bin_path, '-d', '-c', nisd_uuid_to_write, '-v', vdev, '-r', read_operation_ratio_percentage,
                                    '-a', random_seed, '-u', client_uuid, '-Z', request_size_in_bytes,
-                                   '-q', queue_depth, '-N', num_ops), stdout=fp, stderr=fp)
+                                   '-q', queue_depth, '-N', num_ops], stdout=fp, stderr=fp)
 
-    logging.info("return code: ", ps.returncode)
+    out, err = ps.communicate()
     # Sync the log file so all the logs from niova-block-test gets written to log file.
     os.fsync(fp)
 
