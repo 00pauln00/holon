9352cb39f8c6ce2da26ac79b95177342b42aff18
NikhilReddy-2 <nikhil.reddy@paroscale.com>
2023-05-16 11:37:30 +0000

    removed -cov from client applications

--- raftprocess.py
+++ raftprocess.py
@@ -142,19 +142,19 @@ def run_process(fp, raft_uuid, peer_uuid, ptype, app_type, bin_path, base_dir, c
         if app_type == "pumicedb":
             if coalesced_wr == 1:
                 process_popen = subprocess.Popen([bin_path, '-r',
-                                    raft_uuid, '-u', peer_uuid, '-a', '-cov', cluster_params['coverdir']],
+                                    raft_uuid, '-u', peer_uuid, '-a'], 
                                     stdout = fp, stderr = fp)
             elif sync == "1":
                   process_popen = subprocess.Popen([bin_path, '-r',
-                                         raft_uuid, '-u', peer_uuid, '-cov', cluster_params['coverdir']],
+                                         raft_uuid, '-u', peer_uuid],
                                          stdout = fp, stderr = fp)
             else:
                   process_popen = subprocess.Popen([bin_path, '-r',
-                                                      raft_uuid, '-u', peer_uuid, '-a', '-cov', cluster_params['coverdir']],
+                                                      raft_uuid, '-u', peer_uuid, '-a'],
                                                       stdout = fp, stderr = fp)
         elif app_type == "foodpalace" or app_type == "covid":
             process_popen = subprocess.Popen([bin_path, '-r',
-                                    raft_uuid, '-u', peer_uuid, '-cov', cluster_params['coverdir'], '-l', base_dir],
+                                    raft_uuid, '-u', peer_uuid, '-l', base_dir],
                                     stdout = fp, stderr = fp)
         elif app_type == "niovakv":
             log_path = "%s/%s_niovakv_server.log" % (base_dir, peer_uuid)
