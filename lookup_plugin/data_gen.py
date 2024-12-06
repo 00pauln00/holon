98c7c1569af3a60aab326fa329d7f9b85c5dc98b
Nikita Sonsale <nikitassonsale@gmail.com>
2024-12-06 10:11:25 +0000

    updated data gen module to take input params and modified space utilization recipe

--- lookup_plugin/data_gen.py
+++ lookup_plugin/data_gen.py
@@ -11,17 +11,17 @@ class data_generator:
         self.base_path = f"{cluster_params['base_dir']}/{cluster_params['raft_uuid']}"
         self.s3_upload_log = f"{self.base_path}/s3Upload"
 
-    def generate_random_values(self, input_values):
+    def generate_random_values(self, dgen_args):
         dbi_path = get_dir_path(self.cluster_params, DBI_DIR)
 
         if dbi_path != None:
-            json_data = load_parameters_from_json(f"{dbi_path}/{input_values['chunk']}/DV/dummy_generator.json")
-            input_values['chunk'] = str(json_data['TotalChunkSize'])
-            input_values["seqStart"] = str(json_data['SeqEnd'] + 1)
-            input_values["vdev"] = str(json_data['Vdev'])
+            json_data = load_parameters_from_json(f"{dbi_path}/{dgen_args['chunk']}/DV/dummy_generator.json")
+            dgen_args['chunk'] = str(json_data['TotalChunkSize'])
+            dgen_args["seqStart"] = str(json_data['SeqEnd'] + 1)
+            dgen_args["vdev"] = str(json_data['Vdev'])
             dbicount = str(json_data['TMinDbiFileForForceGC'])
         else:
-            input_values["seqStart"] = "0"
+            dgen_args["seqStart"] = "0"
             dbicount = "0"
 
         defaults = {
@@ -43,38 +43,38 @@ class data_generator:
         }
 
         for key, random_val in defaults.items():
-            if key not in input_values:
-                input_values[key] = random_val()
+            if key not in dgen_args:
+                dgen_args[key] = random_val()
     
-        return input_values
+        return dgen_args
 
-    def set_vals_from_json(self, input_values):
+    def set_vals_from_json(self, dgen_args):
         dbi_path = get_dir_path(self.cluster_params, DBI_DIR)
         dbicount = "0"
         if dbi_path != None:
             entries = os.listdir(dbi_path)
-            chunk_no = input_values["chunk"]
+            chunk_no = dgen_args["chunk"]
             if chunk_no not in entries:
                 dbi_path = None
             else:
-                json_data = load_parameters_from_json(f"{dbi_path}/{input_values['chunk']}/DV/dummy_generator.json")
-                input_values["vdev"] = str(json_data['Vdev'])
-                input_values["seqStart"] = str(json_data['SeqEnd'] + 1)
+                json_data = load_parameters_from_json(f"{dbi_path}/{dgen_args['chunk']}/DV/dummy_generator.json")
+                dgen_args["vdev"] = str(json_data['Vdev'])
+                dgen_args["seqStart"] = str(json_data['SeqEnd'] + 1)
                 dbicount = str(json_data['TMinDbiFileForForceGC'])
 
-        return input_values, dbicount
+        return dgen_args, dbicount
 
-    def add_params_to_cmd(self, commands, input_values):
+    def add_params_to_cmd(self, commands, dgen_args):
         for cmd in commands:
-            if 'vdev' in input_values:
-                cmd.extend(['-vdev', input_values['vdev']])
-            if input_values["punchwholechunk"] == "=true":
-                cmd.extend(["-pc", input_values["punchwholechunk"]])
-            if 'strideWidth' in input_values:
-                cmd.extend(["-sw", input_values["strideWidth"]])
-            if 'overlapSeq' in input_values and 'numOfSet' in input_values:
-                cmd.extend(["-se", input_values["overlapSeq"], "-ts", input_values["numOfSet"]])
-        return commands, input_values
+            if 'vdev' in dgen_args:
+                cmd.extend(['-vdev', dgen_args['vdev']])
+            if dgen_args["punchwholechunk"] == "=true":
+                cmd.extend(["-pc", dgen_args["punchwholechunk"]])
+            if 'strideWidth' in dgen_args:
+                cmd.extend(["-sw", dgen_args["strideWidth"]])
+            if 'overlapSeq' in dgen_args and 'numOfSet' in dgen_args:
+                cmd.extend(["-se", dgen_args["overlapSeq"], "-ts", dgen_args["numOfSet"]])
+        return commands, dgen_args
 
     def run_dummy_generator(self, command):
         print("command", command)
@@ -83,7 +83,7 @@ class data_generator:
         except subprocess.CalledProcessError as e:
             print(f"Error: {e}")
 
-    def generate_data(self, input_values, no_of_chunks, is_random, rm_files):
+    def generate_data(self, dgen_args, params):
         bin_path = f'{self.bin_dir}/dummyData'
         s3_config = f'{self.bin_dir}/s3.config.example'
         path = f"{self.base_path}/{DBI_DIR}"
@@ -97,37 +97,37 @@ class data_generator:
                 print(f"An error occurred while creating '{path}': {e}")
 
         dbicount = "0"
-        if is_random:
-            input_values = self.generate_random_values(input_values)
+        if params['is_random']:
+            dgen_args = self.generate_random_values(dgen_args)
         else:
-            input_values, dbicount = self.set_vals_from_json(input_values)
+            dgen_args, dbicount = self.set_vals_from_json(dgen_args)
 
         commands = []
-        for chunk in range(1, no_of_chunks + 1):
-            if no_of_chunks > 1:
-                input_values['chunk'] = str(chunk)
-            command = [bin_path, "-c", input_values['chunk'], "-mp", input_values['maxPunches'], "-mv", input_values['maxVblks'], 
-                       "-p", path, "-pa", input_values['punchAmount'], "-pp", input_values['punchesPer'], "-ps", input_values['maxPunchSize'], 
-                       "-seed", input_values['seed'], "-ss", input_values['seqStart'], "-t", input_values['genType'], "-va", input_values['vbAmount'], 
-                       "-l", "5", "-vp", input_values['vblkPer'], "-bs", input_values['blockSize'], "-bsm", input_values['blockSizeMax'], 
-                       "-vs", input_values['startVblk'], "-s3config", s3_config, "-s3log", self.s3_upload_log, "-dbic", dbicount]
+        for chunk in range(1, params['no_of_chunks'] + 1):
+            if params['no_of_chunks'] > 1:
+                dgen_args['chunk'] = str(chunk)
+            command = [bin_path, "-c", dgen_args['chunk'], "-mp", dgen_args['maxPunches'], "-mv", dgen_args['maxVblks'], 
+                       "-p", path, "-pa", dgen_args['punchAmount'], "-pp", dgen_args['punchesPer'], "-ps", dgen_args['maxPunchSize'], 
+                       "-seed", dgen_args['seed'], "-ss", dgen_args['seqStart'], "-t", dgen_args['genType'], "-va", dgen_args['vbAmount'], 
+                       "-l", "5", "-vp", dgen_args['vblkPer'], "-bs", dgen_args['blockSize'], "-bsm", dgen_args['blockSizeMax'], 
+                       "-vs", dgen_args['startVblk'], "-s3config", s3_config, "-s3log", self.s3_upload_log, "-dbic", dbicount]
             commands.append(command)
 
-        commands, input_values = self.add_params_to_cmd(commands, input_values)
+        commands, dgen_args = self.add_params_to_cmd(commands, dgen_args)
 
-        if is_random:
+        if params['is_random']:
             for cmd in commands:
                 cmd.extend(['-b', 'paroscale-test'])
-                if 'dbiWithPunches' in input_values:
-                    cmd.extend(['-e', input_values['dbiWithPunches']])
-                if not rm_files:
+                if 'dbiWithPunches' in dgen_args:
+                    cmd.extend(['-e', dgen_args['dbiWithPunches']])
+                if not params['rm_files']:
                     cmd.append('-r=true')
 
-        with Pool(processes = no_of_chunks) as pool:
+        with Pool(processes = params['no_of_chunks']) as pool:
             results = pool.map(self.run_dummy_generator, commands)
 
-        if is_random:
-            return input_values['chunk']
+        if params['is_random']:
+            return dgen_args['chunk']
 
 class data_validator:
     def __init__(self, cluster_params):
@@ -159,13 +159,13 @@ class data_validator:
             error_message = f"Process failed with exit code {exit_code}."
             raise RuntimeError(error_message)
     
-    def validate_s3_disk_data(cluster_params, device_path, ublk_uuid, nisd_uuid):
+    def validate_s3_disk_data(self, params):
         bin_path = f'{self.bin_dir}/s3DataValidator'
         log_dir = f'{self.base_path }/s3DV' 
-        nisd_cmdintf_path = "/tmp/.niova/%s" % nisd_uuid  
+        nisd_cmdintf_path = "/tmp/.niova/%s" % params['nisd_uuid']  
         # Ensure log directory exists
         create_directory(log_dir)    
-        cmd = ["sudo", bin_path, '-v', ublk_uuid, '-c', self.s3_config, '-p', log_dir, '-b', 'paroscale-test', '-d', device_path, '-nisdP', nisd_cmdintf_path]
+        cmd = ["sudo", bin_path, '-v', params['ublk_uuid'], '-c', self.s3_config, '-p', log_dir, '-b', 'paroscale-test', '-d', params['device_path'], '-nisdP', nisd_cmdintf_path]
         print(f"s3 dv cmd {cmd}")
         try:
             result = subprocess.run(cmd, check=True)
@@ -178,12 +178,10 @@ class LookupModule(LookupBase):
         cluster_params = kwargs['variables']['ClusterParams']
 
         if operation == "generator":
-            input_values = terms[1]
-            no_of_chunks = terms[2]
-            is_random = terms[3]
-            rm_files = terms[4]
+            dgen_args = terms[1]
+            params = terms[2]
             data = data_generator(cluster_params)
-            chunk = data.generate_data(input_values, no_of_chunks, is_random, rm_files)
+            chunk = data.generate_data(dgen_args, params)
             return chunk
         
         elif operation == "validator":
@@ -192,8 +190,6 @@ class LookupModule(LookupBase):
             dv.validate_data(chunk)
         
         elif operation == "s3_disk_validator":
-            device_path = terms[1]
-            ublk_uuid   = terms[2]
-            nisd_uuid   = terms[3]
+            params = terms[1]
             dv = data_validator(cluster_params)
-            dv.validate_s3_disk_data(cluster_params, device_path, ublk_uuid, nisd_uuid)
+            dv.validate_s3_disk_data(params)
