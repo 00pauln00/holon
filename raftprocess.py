2fde0a0dfe30a7e2673db57638abdfca02cfb8fd
Sudarsan <sudarsan.sivakumar@paroscale.com>
2025-05-27 12:49:49 +0000

    Add log statement for debugging

--- raftprocess.py
+++ raftprocess.py
@@ -87,7 +87,7 @@ def get_executable_path(process_type, app_type, backend_type, binary_dir):
 
 def run_process(fp, raft_uuid, peer_uuid, ptype, app_type, bin_path, base_dir, config_path, node_name, coalesced_wr, sync, cluster_params):
     process_popen = {}
-    print(bin_path)
+    logging.Info(bin_path)
     # binary_dir = os.getenv('NIOVA_BIN_PATH')
     gossipNodes = "%s/gossipNodes" % base_dir
 
